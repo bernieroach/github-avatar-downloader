6868a9ebb7a2b65c0a95ef85b5a0214e90679191
Bernard Roach <bernieroach@gmail.com>
2017-10-10 00:13:39 +0000

    made the code prettier with comments and indenting

--- download_avatars.js
+++ download_avatars.js
@@ -4,72 +4,74 @@ var fs = require('fs');
 var GITHUB_TOKEN = "bda56b65e90dba42e3f11ccfff34b18aa53c8194"
 var GITHUB_USER = 'bernieroach';
 
+// download contributors from a list
 function printContributors(contributorList){
-console.log("hi I am going to print the contributors on the console")
-for (var avatar in contributorList){
+  console.log("hi I am going to print the contributors on the console")
   var pathPrefix = './avatars/';
   var pathPostfix = '.jpeg';
- downloadImageByUrl(contributorList[avatar].avatar_url,`${pathPrefix}${contributorList[avatar].login}${pathPostfix}`);
- }
+// go through list of contibutors
+  for (var avatar in contributorList){
+    // download the image and save
+    downloadImageByUrl(contributorList[avatar].avatar_url,`${pathPrefix}${contributorList[avatar].login}${pathPostfix}`);
+  }
 }
-
+// download a resource and save it in a path
 function downloadImageByUrl(url, filePath){
-request.get(url).on('data',function(data){ /*console.log("DATA", data)*/})
-                        .on('response', function(response){
-                        })
-                        .on('error', function(err){
-                          throw err;
-                        })
-                        .on("end", function(){
-                            console.log("Downloaded" + url + " to " + filePath);
-                        })
-                        .pipe(fs.createWriteStream(filePath));
-                      }
+  request.get(url).on('data',function(data){ /*console.log("DATA", data)*/})
+  .on('response', function(response){
+  })
+  .on('error', function(err){
+    throw err;
+  })
+  .on("end", function(){
+    console.log("Downloaded" + url + " to " + filePath);
+  })
+  .pipe(fs.createWriteStream(filePath));
+}
 
 
 function getRepoContributors(repoOwner, repoName, cb){
-  // this function will get the avatars based on repository owner and repository name
+// this function will get the avatars based on repository owner and repository name
 
+  var jsonStringResponse = "";
+  var apiRepoPrefix = "https://api.github.com/repos/";
+  var apiRepoPostfix = "/contributors";
+  var urlString = `https://${process.env.GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+// create the option object
+  var requestObject = {
+                      url: urlString,
+                      headers: {
+                                "User-Agent": GITHUB_USER
+                      }
+                      }
+  var jsonObject = {};
+
+// repository Owner parameter s required.
   if(!repoOwner){
     console.log("Repository Owner parameter 1 required");
     return;
   }
+// repository Name paameter is required
   if(!repoName){
     console.log("Repository Name parameter 2 required");
     return;
   }
-var jsonStringResponse = "";
-var apiRepoPrefix = "https://api.github.com/repos/";
-var apiRepoPostfix = "/contributors";
-var urlString = `https://${process.env.GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-var requestObject = {
-                    url: urlString,
-                    headers: {
-                              "User-Agent": GITHUB_USER
-                            }
-                    }
 
-var jsonObject = {};
-
-request(requestObject, function(err, response, body){
-  console.log("string in request ", requestObject)
-  if(err){
-    console.log(err);
-    throw err;
-  }
-  console.log("status");
-  console.log(response.statusCode);
-//  console.log(response.body);
-  jsonStringResponse = body;
-  jsonObject = JSON.parse(jsonStringResponse);
-  console.log(jsonObject);
-  console.log("now I have the object - a list of contributors");
-  console.log("pass the list to the call back")
-   cb(jsonObject);
-});
-
+// pass the request object to the request function
+  request(requestObject, function(err, response, body){
+    if(err){
+      console.log(err);
+      throw err;
+    }
+// parse the JSON response to JSON object
+    jsonStringResponse = body;
+    jsonObject = JSON.parse(jsonStringResponse);
+// process the JSON object (list of contributors)
+     cb(jsonObject);
+  });
 };
 
-// run the function.
 
+
+// run the function.
 getRepoContributors(process.argv[2], process.argv[3],printContributors);
