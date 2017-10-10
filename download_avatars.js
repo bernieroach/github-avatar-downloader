48b4dbc398353f8e4e26909b5d01dba4d16a60b9
Bernard Roach <bernieroach@gmail.com>
2017-10-10 00:05:51 +0000

    clean up avatar download js so I can read it better

--- download_avatars.js
+++ download_avatars.js
@@ -3,12 +3,10 @@ var fs = require('fs');
 
 var GITHUB_TOKEN = "bda56b65e90dba42e3f11ccfff34b18aa53c8194"
 var GITHUB_USER = 'bernieroach';
-console.log("You are going to download GitHub avatars soon");
 
 function printContributors(contributorList){
 console.log("hi I am going to print the contributors on the console")
 for (var avatar in contributorList){
-  console.log(contributorList[avatar].avatar_url);
   var pathPrefix = './avatars/';
   var pathPostfix = '.jpeg';
  downloadImageByUrl(contributorList[avatar].avatar_url,`${pathPrefix}${contributorList[avatar].login}${pathPostfix}`);
@@ -18,15 +16,12 @@ for (var avatar in contributorList){
 function downloadImageByUrl(url, filePath){
 request.get(url).on('data',function(data){ /*console.log("DATA", data)*/})
                         .on('response', function(response){
-                          console.log('response status', response.statusCode);
-                          console.log(response.headers['content-type']);
                         })
                         .on('error', function(err){
                           throw err;
                         })
                         .on("end", function(){
-                            console.log("I am at the end");
-                            console.log("downloaded", url);
+                            console.log("Downloaded" + url + " to " + filePath);
                         })
                         .pipe(fs.createWriteStream(filePath));
                       }
@@ -34,6 +29,15 @@ request.get(url).on('data',function(data){ /*console.log("DATA", data)*/})
 
 function getRepoContributors(repoOwner, repoName, cb){
   // this function will get the avatars based on repository owner and repository name
+
+  if(!repoOwner){
+    console.log("Repository Owner parameter 1 required");
+    return;
+  }
+  if(!repoName){
+    console.log("Repository Name parameter 2 required");
+    return;
+  }
 var jsonStringResponse = "";
 var apiRepoPrefix = "https://api.github.com/repos/";
 var apiRepoPostfix = "/contributors";
@@ -44,9 +48,9 @@ var requestObject = {
                               "User-Agent": GITHUB_USER
                             }
                     }
-console.log("string" ,urlString);
+
 var jsonObject = {};
-// I don't know why this doesn't work ... authorization error
+
 request(requestObject, function(err, response, body){
   console.log("string in request ", requestObject)
   if(err){
@@ -66,5 +70,6 @@ request(requestObject, function(err, response, body){
 
 };
 
-// test the function
-getRepoContributors("jquery", "jquery",printContributors);
+// run the function.
+
+getRepoContributors(process.argv[2], process.argv[3],printContributors);
